9a6b8aec61d36ce1dc594ffe508bc591fddfbdc4
MattPC <panmateusz1337@gmail.com>
2018-08-03 17:34:06 +0200

    Added a object to store information to render text

---
+++ objects/textObject.cc
@@ -0,0 +1,35 @@
+#include "objects/textObject.h"
+
+TextObject::TextObject() :
+	position(400.0f, 400.0f, 0.0f),
+	size(100.0f, 100.0f),
+	rotation(0.0f),
+	text("glfwFramework"),
+	sID(ShaderId::Basic)
+{ }
+
+TextObject::TextObject(ShaderId ID, 
+					   const std::string& text, 
+					   const glm::vec3& position, 
+					   const glm::vec2& size, 
+					   float rotation) :
+	position(position),
+	size(size),
+	rotation(rotation),
+	text(text),
+	sID(ID)
+{ }
+
+/* Setters */
+void TextObject::setPosition(glm::vec3 setPosition) { position = setPosition; }
+void TextObject::setSize(glm::vec2 setSize) { size = setSize; }
+void TextObject::setRotation(float setRotation) { rotation = setRotation; }
+void TextObject::setText(const std::string& setText) { text = setText; }
+void TextObject::setShaderId(ShaderId setID) { sID = setID; }
+
+/* Getters */
+const glm::vec3& TextObject::getPosition() const { return position; }
+const glm::vec2& TextObject::getSize() const { return size; }
+float TextObject::getRotation() const { return rotation; }
+const std::string& TextObject::getText() const { return text; }
+ShaderId TextObject::getShaderId() const { return sID; }
---
+++ objects/textObject.h
@@ -0,0 +1,42 @@
+#ifndef TEXTOBJECT_H
+#define TEXTOBJECT_H
+
+#include <string>
+
+#include "utils/GLIncludes.h"
+
+#include "renderable/fontAtlas.h"
+#include "renderable/shaderAtlas.h"
+
+class TextObject
+{
+public:
+	TextObject();
+	TextObject(ShaderId ID, 
+			   const std::string& text, 
+			   const glm::vec3& position, 
+			   const glm::vec2& size, 
+			   float rotation);
+
+	/* Setters */
+	void				setPosition(glm::vec3 setPosition);
+	void				setSize(glm::vec2 setSize);
+	void				setRotation(float setRotation);
+	void				setText(const std::string& setText);
+	void				setShaderId(ShaderId setID);
+
+	/* Getters */
+	const glm::vec3&	getPosition() const;
+	const glm::vec2&	getSize() const;
+	float				getRotation() const;
+	const std::string&	getText() const; 
+	ShaderId			getShaderId() const;
+private:
+	glm::vec3 position;
+	glm::vec2 size;
+	float rotation;
+	std::string text;
+	ShaderId sID;
+};
+
+#endif
